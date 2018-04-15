50bab7f4b546cba37a9d86387e9dbe4e2d6daf54
Jenny Gebske <jennifer@gebske.de>
2018-04-15 14:21:09 +0200

    add button component

---
+++ src/components/MyButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <h2>{{ question }}</h2>
+    <button @click="onClick" class="Button">
+      <slot>Button</slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyButton',
+  props: {
+    title: String,
+    question: String,
+    onClick: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
